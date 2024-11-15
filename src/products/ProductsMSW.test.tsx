79893ac469431bbd12de25e1830f664606382508
François Billet <francois.billet.ext@veolia.com>
2024-11-15 14:44:17 +0100

    MSW: with beforeEach resetHandlers in test to be more explicit

--- src/products/ProductsMSW.test.tsx
+++ src/products/ProductsMSW.test.tsx
@@ -1,7 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import { Products } from "./Products";
+import { server } from "../mocks/node";
+import { handlers } from "../mocks/handlers";
 
 describe("Products", () => {
+  beforeEach(() => {
+    server.resetHandlers(...handlers);
+  });
+
   it("should render 2 products", async () => {
     // When
     render(<Products />);
